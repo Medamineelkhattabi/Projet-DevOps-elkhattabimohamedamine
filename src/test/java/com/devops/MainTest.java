7cae2bdba1fad66c5cc3ec4fdc1f39c4d54a9e4d
mohamed amine <medamine4elkhattabi@gmail.com>
2026-01-06 16:10:26 +0100

    Ajout de l'application Java Maven

---
+++ src/test/java/com/devops/MainTest.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        // Example test case
+        assertTrue(true);
+    }
+}
